3aedc4e48f8ded5ee0d73627320534829895914f
Fabien MARTY <fabien.marty@gmail.com>
2019-04-11 11:48:56 +0200

    feat: introduce mflog_addon
    With this function, we well be able to automatically add some
    context to logs.

--- Makefile
+++ Makefile
@@ -22,6 +22,8 @@ test::
 	cd layers/layer1_python3/0100_mfutil && make test
 	cd layers/layer1_python2/0100_mfutil && make test
 	cd layers/layer1_python3/0300_conf_monitor && make test
+	cd layers/layer1_python3/0500_mflog_addon && make test
+	cd layers/layer1_python2/0500_mflog_addon && make test
 
 directories:
 	@for DIR in tmp opt/python2/lib/python$(PYTHON2_SHORT_VERSION)/site-packages opt/python3/lib/python$(PYTHON3_SHORT_VERSION)/site-packages; do mkdir -p $(MFCOM_HOME)/$${DIR}; done
---
+++ layers/layer1_python3/0500_mflog_addon/mflog_metwork_addon/__init__.py
@@ -0,0 +1,18 @@
+import os
+
+MODULE = os.environ['MODULE']
+CURRENT_PLUGIN_ENV_VAR = "%s_CURRENT_PLUGIN_NAME" % MODULE
+if CURRENT_PLUGIN_ENV_VAR in os.environ:
+    PLUGIN = os.environ[CURRENT_PLUGIN_ENV_VAR]
+else:
+    PLUGIN = "#core#"
+REQUEST_ID_ENV_VAR = "%s_CURRENT_REQUEST_ID" % MODULE
+
+
+def extra_context():
+    extra_context = {
+        "plugin": PLUGIN
+    }
+    if REQUEST_ID_ENV_VAR in os.environ:
+        extra_context["request_id"] = os.environ["REQUEST_ID_ENV_VAR"]
+    return extra_context
