a18d0747d7f7c5a083e9f4aa99d96670a1bb95c9
Fabien MARTY <fabien.marty@gmail.com>
2019-05-10 15:46:31 +0200

    feat: don't use plugin dirname as name

--- adm/_make_crontab.sh
+++ adm/_make_crontab.sh
@@ -15,12 +15,13 @@ export RUNTIME_SUFFIX
 cat "${MODULE_HOME}/config/crontab" |envtpl
 
 if test -d "${MODULE_RUNTIME_HOME}/var/plugins"; then
-    for dir in ${MODULE_RUNTIME_HOME}/var/plugins/*; do
-        if test -s "${dir}/crontab"; then
-            BDIR=$(basename "${dir}")
-            echo "##### BEGINNING OF METWORK ${MODULE} PLUGIN ${BDIR} CRONTAB #####"
-            cat "${dir}/crontab" | envtpl
-            echo "##### END OF METWORK ${MODULE} PLUGIN ${BDIR} CRONTAB #####"
+    for TMP in $(plugins.list --raw); do
+        BDIR=$(echo "${TMP}" |awk -F '~~~' '{print $4;}')
+        NAME=$(echo "${TMP}" |awk -F '~~~' '{print $1;}')
+        if test -s "${BDIR}/crontab"; then
+            echo "##### BEGINNING OF METWORK ${MODULE} PLUGIN ${NAME} CRONTAB #####"
+            cat "${BDIR}/crontab" | envtpl
+            echo "##### END OF METWORK ${MODULE} PLUGIN ${NAME} CRONTAB #####"
         fi
     done
 fi
