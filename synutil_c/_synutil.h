23e71605e02a55715fd9040654dd84d74a27155f
Fabien MARTY <fabien.marty@gmail.com>
2019-08-05 15:21:13 +0200

    feat: move synutil_c and synutil_lua to mfext

--- synutil_c/_synutil.h
+++
@@ -1,8 +0,0 @@
-#ifndef _SYNUTIL_H_
-#define _SYNUTIL_H_
-
-#include <glib.h>
-
-void synutil_echo_something(const gchar *code, const gchar *before, const gchar *after, const gchar *message, const gchar *line_end);
-
-#endif /* _SYNUTIL_H_ */
