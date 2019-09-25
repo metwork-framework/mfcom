6f7da95da679e05bc2382d054add594f0bc0bbcd
Fabien MARTY <fabien.marty@gmail.com>
2019-09-25 15:41:33 +0200

    fix: fix some issues with node plugins

--- adm/plugin.mk
+++ adm/plugin.mk
@@ -1,4 +1,4 @@
-.PHONY: freeze
+.PHONY: freeze clean prerelease_check all superclean
 
 NAME:=$(shell cat .layerapi2_label |sed 's/^plugin_//g' |awk -F '@' '{print $$1;}')
 VERSION:=$(shell config.py config.ini general version |sed "s/{{MFMODULE_VERSION}}/$${MFMODULE_VERSION}/g")
@@ -11,9 +11,6 @@ ifneq ("$(wildcard python3_virtualenv_sources/requirements-to-freeze.txt)","")
 else
 	REQUIREMENTS3:=
 endif
-ifneq ("$(wildcard python3_virtualenv_sources/prerequirements-to-freeze.txt)","")
-	REQUIREMENTS3:=python3_virtualenv_sources/prerequirements3.txt $(REQUIREMENTS3)
-endif
 ifneq ("$(REQUIREMENTS3)","")
 	PREREQ+=$(REQUIREMENTS3) python3_virtualenv_sources/src
 	DEPLOY+=local/lib/python$(PYTHON3_SHORT_VERSION)/site-packages/requirements3.txt
@@ -23,15 +20,14 @@ ifneq ("$(wildcard python2_virtualenv_sources/requirements-to-freeze.txt)","")
 else
 	REQUIREMENTS2:=
 endif
-ifneq ("$(wildcard python2_virtualenv_sources/prerequirements-to-freeze.txt)","")
-	REQUIREMENTS2:=python2_virtualenv_sources/prerequirements2.txt $(REQUIREMENTS2)
-endif
 ifneq ("$(REQUIREMENTS2)","")
 	PREREQ+=$(REQUIREMENTS2) python2_virtualenv_sources/src
 	DEPLOY+=local/lib/python$(PYTHON2_SHORT_VERSION)/site-packages/requirements2.txt
 endif
 ifneq ("$(wildcard package.json)","")
 	PREREQ+=package-lock.json
+	PREREQ+=node_modules
+    DEPLOY+=local/lib/node_modules
 endif
 LAYERS=$(shell cat .layerapi2_dependencies |tr '\n' ',' |sed 's/,$$/\n/')
 
@@ -51,7 +47,7 @@ custom::
 	@echo "override me" >/dev/null
 
 superclean: clean
-	rm -Rf python?_virtualenv_sources/requirements?.txt python?_virtualenv_sources/prerequirements?.txt package-lock.json
+	rm -Rf python?_virtualenv_sources/requirements?.txt package-lock.json
 
 local/lib/python$(PYTHON3_SHORT_VERSION)/site-packages/requirements3.txt: $(REQUIREMENTS3) python3_virtualenv_sources/src
 	_install_plugin_virtualenv $(NAME) $(VERSION) $(RELEASE)
@@ -69,24 +65,12 @@ python3_virtualenv_sources/requirements3.txt: python3_virtualenv_sources/require
 python2_virtualenv_sources/requirements2.txt: python2_virtualenv_sources/requirements-to-freeze.txt
 	cd python2_virtualenv_sources && layer_wrapper --empty --layers=$(LAYERS) -- freeze_requirements requirements-to-freeze.txt >requirements2.txt
 
-python3_virtualenv_sources/prerequirements3.txt: python3_virtualenv_sources/prerequirements-to-freeze.txt
-	cd python3_virtualenv_sources && layer_wrapper --empty --layers=$(LAYERS) -- freeze_requirements prerequirements-to-freeze.txt >prerequirements3.txt
-
-python2_virtualenv_sources/prerequirements2.txt: python2_virtualenv_sources/prerequirements-to-freeze.txt
-	cd python2_virtualenv_sources && layer_wrapper --empty --layers=$(LAYERS) -- freeze_requirements prerequirements-to-freeze.txt >prerequirements2.txt
-
 python3_virtualenv_sources/src: $(REQUIREMENTS3)
-	if test -f python3_virtualenv_sources/prerequirements3.txt; then \
-	    cd python3_virtualenv_sources && layer_wrapper --empty --layers=$(LAYERS) -- download_compile_requirements prerequirements3.txt; \
-	fi
 	if test -f python3_virtualenv_sources/requirements3.txt; then \
 	    cd python3_virtualenv_sources && layer_wrapper --empty --layers=$(LAYERS) -- download_compile_requirements requirements3.txt; \
 	fi
 
 python2_virtualenv_sources/src: $(REQUIREMENTS2)
-	if test -f python2_virtualenv_sources/prerequirements2.txt; then \
-	    cd python2_virtualenv_sources && layer_wrapper --empty --layers=$(LAYERS) -- download_compile_requirements prerequirements2.txt; \
-	fi
 	if test -f python2_virtualenv_sources/requirements2.txt; then \
 	    cd python2_virtualenv_sources && layer_wrapper --empty --layers=$(LAYERS) -- download_compile_requirements requirements2.txt; \
 	fi
@@ -95,7 +79,19 @@ package-lock.json: package.json
 	rm -f $@
 	export LAYERAPI2_LAYERS_PATH=`pwd`:$(LAYERAPI2_LAYERS_PATH) ; plugin_wrapper $(NAME) -- npm install
 
-release: clean $(PREREQ) custom
+node_modules: package-lock.json
+	rm -Rf node_modules
+	export LAYERAPI2_LAYERS_PATH=`pwd`:$(LAYERAPI2_LAYERS_PATH) ; plugin_wrapper $(NAME) -- npm install
+
+local/lib/node_modules:
+	rm -f local/lib/node_modules
+	mkdir -p local/lib
+	if test -d node_modules; then cd local/lib && ln -s ../../node_modules node_modules; fi
+
+prerelease_check:
+	@N=`plugins.info $(NAME) 2>/dev/null |wc -l` ; if test $${N} -gt 0; then echo "ERROR: please uninstall the plugin before doing 'make release'" ; exit 1; fi
+
+release: prerelease_check clean $(PREREQ) custom
 	layer_wrapper --empty --layers=$(LAYERS),python3@mfcom -- _plugins.make --show-plugin-path
 
 develop: $(PREREQ) custom $(DEPLOY)
